da5901fc459fb8da24bec9a9cf0c4a4c56e6ed86
Aurino Geraldo <aurinoaj@gmail.com>
2025-05-14 08:23:14 -0300

    Botao e widget colapsavel

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,23 @@
+import type { ButtonHTMLAttributes } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const buttonVariants = tv({
+  base: "text-zinc-400 rounded-lg hover:text-zinc-100 hover:bg-zinc-800 disabled:opacity-50 disabled:pointer-events-none cursor-pointer",
+  variants: {
+    size: {
+      default: "px-3 py-2",
+      icon: "p-2",
+      "icon-sm": "p-1",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+export type TButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>;
+
+export function Button({ size, className, ...props }: TButtonProps) {
+  return <button className={buttonVariants({ size, className })} {...props} />;
+}
--- src/components/upload-widget/upload-widget.tsx
+++ src/components/upload-widget/upload-widget.tsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetList } from "./upload-widget-list";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export const UploadWidget = () => {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
   return (
-    <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape">
-      <UploadWidgetHeader />
+    <Collapsible.Root
+      onOpenChange={setIsWidgetOpen}
+      className="w-full flex items-center justify-center"
+    >
+      <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape">
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-      <div className="flex flex-col gap-4 py-3">
-        <UploadWidgetDropzone />
-
-        <div className="h-px bg-zinc-800/75" />
-
-        <UploadWidgetList />
+        <Collapsible.Content>
+          <UploadWidgetHeader />
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
+            <div className="h-px bg-zinc-800/75" />
+            <UploadWidgetList />
+          </div>
+        </Collapsible.Content>
       </div>
-    </div>
+    </Collapsible.Root>
   );
 };
