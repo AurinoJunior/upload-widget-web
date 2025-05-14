d1200f67c73b65dc6b7e780cd6d191c532cf2f4b
Aurino Geraldo <aurinoaj@gmail.com>
2025-05-14 08:54:45 -0300

    Componente: Lista de uploads

--- src/components/upload-widget/upload-widget-list.tsx
+++ src/components/upload-widget/upload-widget-list.tsx
@@ -1,3 +1,16 @@
+import { UploadWidgetItem } from "./upload-widget-item";
+
 export function UploadWidgetList() {
-  return <div className="px-3 h-48"></div>;
+  return (
+    <div className="px-3 flex flex-col gap-3">
+      <span className="text-xs font-medium">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+
+      <div className="flex flex-col gap-2">
+        <UploadWidgetItem />
+        <UploadWidgetItem />
+      </div>
+    </div>
+  );
 }
