ea5b55c8f01d56bdf3818c0f79a05d0807d3137c
Aurino Geraldo <aurinoaj@gmail.com>
2025-05-23 15:21:29 -0300

    Estados Visuais nos Componentes

--- src/components/upload-widget/upload-widget-list.tsx
+++ src/components/upload-widget/upload-widget-list.tsx
@@ -1,16 +1,22 @@
 import { UploadWidgetItem } from "./upload-widget-item"
 
 export function UploadWidgetList() {
+  const isUploadListEmpty = false
+
   return (
     <div className="px-3 flex flex-col gap-3">
       <span className="text-xs font-medium">
         Uploaded files <span className="text-zinc-400">(2)</span>
       </span>
 
-      <div className="flex flex-col gap-2">
-        <UploadWidgetItem />
-        <UploadWidgetItem />
-      </div>
+      {isUploadListEmpty ? (
+        <span className="text-xs text-zinc-400">No uploads added</span>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <UploadWidgetItem />
+          <UploadWidgetItem />
+        </div>
+      )}
     </div>
   )
 }
